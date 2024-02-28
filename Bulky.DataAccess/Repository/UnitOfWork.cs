c5f8ec06ea52419f60a03c6f20fcff504d04fc32
Wesley Queiroz <wesleygomesqueiroz@gmail.com>
2024-02-28 19:39:06 -0300

    Assignment 1 Solution - Product Repository and UnitOfWork

--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -12,11 +12,13 @@ namespace Bulky.DataAccess.Repository
     {
         private readonly ApplicationDbContext _db;
         public ICategoryRepository Category {  get; set; }
+        public IProductRepository Product {  get; set; }
 
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             Category = new CategoryRepository(_db);
+            Product = new ProductRepository(_db);
         }
 
         public void Save()
