8c0dae6af8b20edcd8ccfad3023df47d64b47441
Wesley Queiroz <wesleygomesqueiroz@gmail.com>
2024-04-01 20:45:54 -0300

    Authorization in Project

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -14,6 +14,12 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.ConfigureApplicationCookie(opstions =>
+{
+    opstions.LoginPath = $"/Identity/Account/Login";
+    opstions.LogoutPath = $"/Identity/Account/Logout";
+    opstions.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
